4673d7c0362c5bc67293f7e9438f52436067fe69
atul-shr <atulsharma131@gmail.com>
2022-01-15 22:39:20 +0530

    added functions

---
+++ src/binary_tree.py
@@ -0,0 +1,17 @@
+import math
+
+test = {
+    "input": {
+        "cards": [13, 12, 11, 7, 6, 5, 3, 2, 1],
+        "query": 7
+    },
+    "output": 3
+}
+
+
+def locate_cards(cards, query):
+    pass
+
+
+print(locate_cards(**test["input"]) == test["output"])
+
