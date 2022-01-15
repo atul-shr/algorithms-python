49c59132b8d2ca7a4caa229449594f6ce8e624b9
atul-shr <atulsharma131@gmail.com>
2022-01-15 22:52:36 +0530

    added new empty data handling in func

--- src/binary_tree.py
+++ src/binary_tree.py
@@ -1,26 +1,30 @@
 import math
 
-test = {
+tests = [{
     "input": {
         "cards": [13, 12, 11, 7, 6, 5, 3, 2, 1],
         "query": 7
     },
     "output": 3
-}
+},{
+    "input": {
+        "cards": [],
+        "query": 3
+    },
+    "output": -1
+},
+]
 
 
 def locate_cards(cards, query):
     position = 0
 
-    while True:
+    while position < len(cards):
         if cards[position] == query:
             return position
-
-        if position == len(cards):
-            return -1
-
         position += 1
+    return -1
 
-
-print(locate_cards(**test["input"]) == test["output"])
+for test in tests:
+    print(locate_cards(**test["input"]) == test["output"])
 
